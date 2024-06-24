bc46b4a5ca98995cdbce5fff9c2ee0fce0df0d02
Lucas Lima <lucaslima_miml@hotmail.com>
2024-06-24 20:53:21 -0300

    refactor(main): remove event listener used for tests

--- src/main/java/com/lucaslima/dynamicmapper/DynamicmapperApplication.java
+++ src/main/java/com/lucaslima/dynamicmapper/DynamicmapperApplication.java
@@ -1,44 +1,14 @@
 package com.lucaslima.dynamicmapper;
 
-import com.lucaslima.dynamicmapper.core.ports.in.MapperUseCase;
-import com.lucaslima.dynamicmapper.dto.Client;
-import com.lucaslima.dynamicmapper.dto.Email;
-import com.lucaslima.dynamicmapper.dto.Enterprise;
-import com.lucaslima.dynamicmapper.dto.Person;
-import com.lucaslima.dynamicmapper.dto.Worker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import java.time.LocalDate;
-import java.time.ZoneOffset;
 
 @SpringBootApplication
 public class DynamicmapperApplication {
 
-    @Autowired
-    MapperUseCase mapperUseCase;
 
     public static void main(String[] args) {
         SpringApplication.run(DynamicmapperApplication.class, args);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void runAfterStartup() throws Exception {
-        Object object = new Person(
-                "Nome do Fulano",
-                LocalDate.parse("1990-01-01").atStartOfDay().toInstant(ZoneOffset.UTC),
-                "01234567890",
-                new Email("teste@teste.com"),
-                "123456789",
-                "M"
-        );
-
-        System.out.println(mapperUseCase.map(object, Client.class));
-        System.out.println(mapperUseCase.map(object, Worker.class));
-        System.out.println(mapperUseCase.map(object, Enterprise.class));
-
-    }
 }
